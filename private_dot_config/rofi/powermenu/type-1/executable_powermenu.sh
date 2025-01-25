fbca16cc8af5553bed8a3462322ec1602e453f9e
alkuinvito <alkuinvito@tutanota.com>
2025-01-25 19:27:52 +0700

    init commit

---
+++ private_dot_config/rofi/powermenu/type-1/executable_powermenu.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+## Author : Aditya Shakya (adi1090x)
+## Github : @adi1090x
+#
+## Rofi   : Power Menu
+#
+## Available Styles
+#
+## style-1   style-2   style-3   style-4   style-5
+
+# Current Theme
+dir="$HOME/.config/rofi/powermenu/type-1"
+theme='style-1'
+
+# CMDs
+uptime="`uptime -p | sed -e 's/up //g'`"
+host=`hostname`
+
+# Options
+shutdown=' Shutdown'
+reboot=' Reboot'
+lock=' Lock'
+suspend='󰒲 Suspend'
+logout='󰗽 Logout'
+
+# Rofi CMD
+rofi_cmd() {
+	rofi -dmenu \
+		-p "$host" \
+		-mesg "Uptime: $uptime" \
+		-theme ${dir}/${theme}.rasi
+}
+
+# Pass variables to rofi dmenu
+run_rofi() {
+	echo "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+}
+
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
+    $shutdown)
+		systemctl poweroff
+        ;;
+    $reboot)
+		systemctl reboot
+        ;;
+    $lock)
+    # loginctl lock-session
+    betterlockscreen -l dim
+        ;;
+    $suspend)
+		mpc -q pause
+		amixer set Master mute
+		systemctl suspend
+        ;;
+    $logout)
+		i3-msg exit
+        ;;
+esac
---
+++ private_dot_config/wezterm/executable_wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.color_scheme = "Catppuccin Macchiato"
+config.font = wezterm.font("JetBrains Mono")
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.window_decorations = "TITLE | RESIZE"
+config.window_background_opacity = 0.90
+
+return config
