8a18d7c481e52decf6676e1c3a336cb74babbaa8
alkuinvito <alkuinvito@tutanota.com>
2025-07-17 20:40:20 +0700

    Update .zshrc Update .config/hypr/hyprland.conf Update .config/nvim/init.lua Update .config/waybar/style.css Update .config/zed/settings.json

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,6 +1,7 @@
 require("config.lazy")
-vim.cmd("set number")
-vim.cmd("set cmdheight=0")
+vim.opt.cmdheight=0
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
