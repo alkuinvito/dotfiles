bbeb07f0b4402d33a4db17a37a1bbec89774ca66
alkuinvito <alkuinvito@tutanota.com>
2025-08-12 20:38:36 +0700

    Add .config/waytrogen/config.json Add .config/waytrogen/postchange.sh

---
+++ private_dot_config/waytrogen/executable_postchange.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Waytrogen post-change hook
+# Waytrogen passes the wallpaper path as the first argument ($1)
+
+WALLPAPER_PATH="$2"
+
+# Make sure the path is valid
+[ -z "$WALLPAPER_PATH" ] && exit 1
+[ ! -f "$WALLPAPER_PATH" ] && exit 1
+
+# Update colorscheme with Wallust
+wallust run "$WALLPAPER_PATH"
